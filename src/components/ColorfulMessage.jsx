072720c809be31eba38e0fc78a70ae29279b5683
masattv <k3absorb430@gmail.com>
2025-04-02 07:17:30 +0900

    ～section6 No.48

---
+++ src/components/ColorfulMessage.jsx
@@ -0,0 +1,11 @@
+export const ColorfulMessage = (props) => {
+    const { color, children } = props;
+    const contentStyleA = {
+        color,
+        fontSize: "18px",
+    };
+
+    return(
+        <p style={contentStyleA}>{children}</p>
+    )
+}
